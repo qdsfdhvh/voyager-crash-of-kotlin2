4d5bc0235044a24c0337f93735e53b5d65591f87
seiko <usseiko@MacBook-Pro-14.local>
2024-05-12 10:11:33 +0800

    add voyager crash codes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,15 @@ android {
     }
 
     buildTypes {
+        getByName("debug") {
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -31,17 +40,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 composeCompiler {
@@ -56,6 +66,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.bundles.voyager)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/seiko/demo/voyager_crash_of_kotlin2/MainActivity.kt
+++ app/src/main/java/com/seiko/demo/voyager_crash_of_kotlin2/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import cafe.adriel.voyager.transitions.SlideTransition
 import com.seiko.demo.voyager_crash_of_kotlin2.ui.theme.Voyagercrashofkotlin2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Voyagercrashofkotlin2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Navigator(HomeScreen) { navigator ->
+                    SlideTransition(navigator)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+object HomeScreen : Screen {
+    private fun readResolve(): Any = HomeScreen
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Scaffold { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+            ) {
+                Button(onClick = { navigator.push(DetailScreen) }) {
+                    Text("detail (crash)")
+                }
+                Button(onClick = { navigator.push(DataDetailScreen) }) {
+                    Text("detail")
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Voyagercrashofkotlin2Theme {
-        Greeting("Android")
+object DetailScreen : Screen {
+    private fun readResolve(): Any = DetailScreen
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Scaffold { innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Button(onClick = { navigator.pop() }) {
+                    Text("back")
+                }
+            }
+        }
+    }
+}
+
+data object DataDetailScreen : Screen {
+    private fun readResolve(): Any = DataDetailScreen
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Scaffold { innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Button(onClick = { navigator.pop() }) {
+                    Text("back")
+                }
+            }
+        }
     }
 }
